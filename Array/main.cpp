b80158a3d17ddab158c5f5e92496546d4a31b668
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-02-19 04:15:19 +0200

    Added 2nd preserved driver program for Array library.
    Original timestamps:
    main.cpp:                                               21/10/2008  6:38:06pm

--- Array/main.cpp
+++ Array/main.cpp
@@ -14,56 +14,34 @@ using std::endl;
 using std::ofstream;
 
 #include <cstdlib>
+#include <math.h>
+#include <time.h>
+
+#include <vector>
+using std::vector;
 
 #include "Array/Array.h"
 using namespace ASL::DataStructures;
+#include "utility.h"
+using namespace ASL::Utility;
 
 
-double dv(double x)
-{
-	return 2*x;
-}
 
 
 int main()
 {
-	ofstream outFile("print.txt");
+	int a[6] = {1,2,3,4,5,6};
+	int b[6] = {1,2,3,4,5,6};
 
-	Array<double> v_5(5,0.1);
-	Array<Array<double> > A_3x2(3,2,0.2);
-	Array<Array<Array<double> > > B_4x3x2(4,3,2,0.3);
-	Array<Array<Array<double> > > C_4x3x2(4,A_3x2);
-	Array<Array<Array<double> > > C_4x4x5(4,4,v_5);
+	Array<int> A(a,6);
+	Array<int> B(b,6);
+
+	cout<<compare(A,B,0,5)<<endl;
 
 
 
-	// format output
-	Array<Array<double> >::delimiter = "\n";
-	Array<Array<Array<double> > >::delimiter = "\n\n";
-	
-	// print arrays.
-	outFile<<"v_5\n\n"<<v_5<<"\n\n>dv\n\n";
-	v_5.map(dv);
-	outFile<<v_5<<"\n\nA_3x2.\n\n";
 
-	outFile<<A_3x2<<"\n\n>dv\n\n";
-	A_3x2.map(dv);
-	outFile<<A_3x2<<"\n\nB_4x3x2.\n\n";
-
-	outFile<<B_4x3x2<<"\n\n>dv\n\n";
-	B_4x3x2.map(dv);
-	outFile<<B_4x3x2<<"\n\n>C_4x3x2.\n\n";
-
-	outFile<<C_4x3x2<<"\n\n>dv\n\n";
-	C_4x3x2.map(dv);
-	outFile<<C_4x3x2<<"\n\nC_4x4x5.\n\n";
-
-	outFile<<C_4x4x5<<"\n\n>dv\n\n";
-	C_4x4x5.map(dv);
-	outFile<<C_4x4x5<<"\n\n";
-	
-
-	system("PAUSE");
+	//system("PAUSE");
 	return 0;
 } // end function main
 
